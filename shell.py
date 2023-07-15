5207de8ded20a0a387eff974f5f66e7f3b1da55c
gugu256 <gugu256@mail.com>
2023-07-15 16:30:29 +0200

    I had to copy the whole sh*t

--- shell.py
+++ shell.py
@@ -1,12 +1,8 @@
-#########
-# SHELL #
-#########
-
 import g
 
 while True:
-    text = input("G >>> ")
-    result, error = g.run("<stdin>", text)
+		text = input('G >>> ')
+		result, error = g.run('<stdin>', text)
 
-    if error: print(error.as_string())
-    else: print(result)
+		if error: print(error.as_string())
+		else: print(result)
